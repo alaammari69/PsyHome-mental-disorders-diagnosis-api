00b1c7c9b3bf59eb1b09d3e07c434aa6f8318294
Ala Ammeri <ala.ammeri@proton.me>
2026-05-12 17:43:46 +0100

    done! finished! finally ;) but still needs testing

--- agents/test_tools.py
+++
@@ -1,5 +0,0 @@
-from custom_tools import get_related_disorders_and_symptoms
-from rich import print
-
-print(get_related_disorders_and_symptoms([1,2,25]))
-
--- load_embedder.py
+++
@@ -1,9 +0,0 @@
-from embedder.embedders import HuggingFaceEmbedder
-import time
-
-HuggingFaceEmbedder.get_embedder()
-
-
-while True:
-    time.sleep(60)
-
